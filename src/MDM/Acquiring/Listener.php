caf8aaf064f6c6788b949a3d1893eaab8c1175eb
=favor1t@yandex.ru <=favor1t@yandex.ru>
2015-12-02 12:10:29 +0300

    MDMBank

--- src/MDM/Acquiring/Listener.php
+++ src/MDM/Acquiring/Listener.php
@@ -4,8 +4,16 @@ namespace MDM\Acquiring;
 use MDM\Acquiring\DBOrder as MDB;
 use MDM\Acquiring\Callback as CallBack;
 
+/**
+ * 
+ */
 class Listener
 {
+        /**
+         * 
+         * @param type $callback
+         * @return boolean
+         */
 	public function __construct($callback)
 	{
             if ( !is_array($callback) && empty($callback['RC']) && empty($callback['Order']) ){
@@ -15,6 +23,10 @@ class Listener
             $this->record($callback);
 	}
 
+        /**
+         * 
+         * @param array $callback
+         */
 	protected function record(Array $callback)
 	{
             if( isset( $callback['RC'] ) && isset( $callback['Order'] ) ){
@@ -25,13 +37,24 @@ class Listener
                 $this->insertLog($callback['Order'],$callback);
             }
 	}
-
+        
+        /**
+         * 
+         * @param type $order_id
+         * @param type $callback
+         * @return type
+         */
 	protected function updateOrder($order_id , $callback)
 	{
             $MDM = new MDB();
             return $MDM->updateOrder($order_id, array_change_key_case($callback, CASE_LOWER));
 	}
 
+        /**
+         * 
+         * @param type $order_id
+         * @param type $callback
+         */
 	protected function insertLog($order_id , $callback)
 	{
             new CallBack($order_id,$callback);
